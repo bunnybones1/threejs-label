61a931c2f930601c15b8b7347a561a4c1d33c55e
Tomasz Dysinski <tom@jam3.com>
2016-01-29 12:03:12 -0500

    fixed BG, added alignment and fixed bower dependency version

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function Label(text, properties) {
 		height: 0,
 		size: 16,
 		hover: 0.30,
-		color: 0xffffff,
 
 		curveSegments: 0,
 
@@ -14,56 +13,63 @@ function Label(text, properties) {
 		bevelSegments: 3,
 		bevelEnabled: false,
 
-		// font: "cranberry gin", // helvetiker, optimer, gentilis, droid sans, droid serif
-		font: "systematic j", // helvetiker, optimer, gentilis, droid sans, droid serif
-		weight: "normal", // normal bold
-		style: "normal", // normal italic
+		color: 0xffffff,
+		bg: false,
+		bgColor: 0x7f7f7f,
+		bgAlpha: 0.5,
+		bgMargin: 10,
+
+		// font: 'cranberry gin', // helvetiker, optimer, gentilis, droid sans, droid serif
+		font: 'systematic j', // helvetiker, optimer, gentilis, droid sans, droid serif
+		weight: 'normal', // normal bold
+		style: 'normal', // normal italic
 
 		material: 0,
 		extrudeMaterial: 1,
-		alignX: 0.5,
+		alignX: 0,
 		alignY: 0.5
 	});
 
 
 	if(!properties.material) {
 		properties.material = new THREE.MeshBasicMaterial({
-			color: properties.color,
-			lights: false,
-			fog: false,
-			// wireframe: true
+			color: properties.color
 		});
 	}
 
-	if(!properties.bgMaterial) {
+	if(!properties.bgMaterial && properties.bg) {
 		properties.bgMaterial = new THREE.MeshBasicMaterial({
-			color: 0xffffff,
-			emissive: 0xffffff,
-			lights: false,
-			fog: false,
+			color: properties.bgColor,
 			transparent: true,
-			opacity: 0.2
+			opacity: 0.5,
+			side: THREE.DoubleSide
 		});
 	}
 
 	function generateBG() {
-		if(properties.bg === true) {
-			properties.bg = new THREE.Mesh(planeGeom, properties.bgMaterial);
-			this.add(properties.bg);
-		}
 		if(properties.bg) {
-			var textBounds = this.textMesh.geometry.boundingBox;
-			var planeWidth = textBounds.max.x - textBounds.min.x;
-			var planeHeight = textBounds.max.y - textBounds.min.y;
-			var planeGeom = new THREE.PlaneGeometry(planeWidth, planeHeight);
-			properties.bg.position.x = textBounds.min.x + planeWidth * .5;
-			properties.bg.position.y = textBounds.min.y + planeHeight * .5;
+			var textBounds = this.textMesh.geometry.boundingBox.clone();
+			var halfHeightOfText = textBounds.size().y * 0.5;
+			textBounds.expandByScalar(properties.bgMargin);
+			var size = textBounds.size();
+			if(properties.bg === true) {
+				var planeGeom = new THREE.PlaneGeometry(1, 1);
+				properties.bg = new THREE.Mesh(planeGeom, properties.bgMaterial);
+				this.add(properties.bg);
+				this.bg = properties.bg;
+			}
+			properties.bg.scale.set(size.x, size.y, 1);
+			properties.bg.position.x = textBounds.min.x + size.x * 0.5;
+			properties.bg.position.y = -textBounds.min.y - size.y * 0.5;
+			properties.bg.position.z = -1;
 		}
 	}
 
 	Object.defineProperty(this, 'text', {
 		set: function(value) {
-			if(value === this._text) return;
+			if(value === this._text) {
+				return;
+			}
 			this._text = value;
 			if(this.textMesh) {
 				this.remove(this.textMesh);
@@ -71,17 +77,23 @@ function Label(text, properties) {
 			}
 
 			var textGeo = new THREE.TextGeometry(value, properties);
+			textGeo.computeBoundingBox();
+			var size = textGeo.boundingBox.size();
+			textGeo.vertices.forEach(function(vertex){
+				vertex.x -= size.x * properties.alignX;
+				vertex.y -= size.y * properties.alignY;
+			});
+			textGeo.computeBoundingBox();
 			this.textMesh = new THREE.Mesh(textGeo, properties.material);
 			this.add(this.textMesh);
 			this.textMesh.rotation.x = Math.PI;
-			textGeo.computeBoundingBox();
 			textGeo.computeVertexNormals();
 			generateBG.call(this);
 		},
 		get: function() {
 			return this._text;
 		}
-	})
+	});
 	THREE.Object3D.call(this);
 	this.text = text;
 
